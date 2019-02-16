230126c74b131fe83a79fd622ccdc94fbbd671f1
Vinh Nguyen <vinhhnguyen@cmail.carleton.ca>
2019-02-16 17:19:27 -0500

    added syntax

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var wordBank1 = ["Chat App", "Dating App", "Website", "Platformer Game", "Shooter Game"]
-var wordBank2 = ["Kids", "Teens", "Students", "Grandparents", "Parents"]
+var wordBank1 = ["Chat App", "Dating App", "Website", "Platformer Game", "Shooter Game"];
+var wordBank2 = ["Kids", "Teens", "Students", "Grandparents", "Parents"];
 
 function randInt(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 function generateIdea() {
-    var roll1 = randInt(0, 5)
-    var roll2 = randInt(0, 5)
-    document.getElementById("form1").innerHTML = wordBank1[roll1]
-    document.getElementById("form2").innerHTML = wordBank2[roll2]
+    var roll1 = randInt(0, 5);
+    var roll2 = randInt(0, 5);
+    document.getElementById("form1").innerHTML = wordBank1[roll1];
+    document.getElementById("form2").innerHTML = wordBank2[roll2];
 }
