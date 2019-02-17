7a4bed987e1875687874fac3c25640946fb9bcad
FruityToasters <fardeen2000@hotmail.com>
2019-02-17 05:14:46 -0500

    Working on the parse javascript file which focusses on making friendly logical expressions for the truthtable file

--- parser.js
+++ parser.js
@@ -1,39 +1,42 @@
 
 qnttyLetters = []
-var exp = document.getElementById("expression");
+var expression = document.getElementById("expression");
 
-for(var i = 1; l = exp; i < l){
+function parse(){
 
-    if (isLetter(exp.charAt(i-1)) && isLetter(exp.charAt(i))){
-        //TELL USER TO FUCK OFF
+    var exp = expression.prototype.limit()
+
+    for(var i = 1; l = exp; i < l){
+
+        if (isLetter(exp.charAt(i-1)) && isLetter(exp.charAt(i))){
+            //TELL USER TO FUCK OFF
+        }
+    }
+
+    for (var i = 0; l = exp; i < l){
+        if (isLetter(exp.charAt(i))){
+            qnttyLetters += exp.charAt(i);
+        }
     }
 }
-
-for(var i = 1; l = exp; i < l){
-
-    if (isLetter(exp.charAt(i-1)) && isLetter(exp.charAt(i))){
-        //TELL USER TO FUCK OFF
-    }
-}
-
-for (var i = 0; l = exp; i < l){
-    if (isLetter(exp.charAt(i))){
-        qnttyLetters += exp.charAt(i);
-    }
-}
-
-
-
-
-
-
-
-
 
 function isLetter(str) {
     return str.length === 1 && str.match(/[a-z]/i);
 }
 
-function hasConjunction(){
+function hasConjunction(str){
+    if (str.charAt(0) === '\\' ||str.charAt(0) === '\,' || str.charAt(0) ==='\/' ){
+        //TELL USER TO FUCK OFF
+    }
+    for(var i = 1; l = str; i < l){
+
+        if ( (str[2*i] === '\/' && str[2*i] !== '\\') || (str[2*i] === '\\' && str[2*i] !== '\/') ){
+            //TELL USER TO FUCK OFF
+        }
+        if (isLetter(str.charAt(i-1)) && isLetter(str.charAt(i))){
+            //TELL USER TO FUCK OFF
+        }
+    }
     
+
 }
